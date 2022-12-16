6d3f402e6cc1c13206cb14070b3d3ff1b9812ed9
Anatolii Rotckin <rokkker@yandex.ru>
2022-12-16 15:10:43 +0200

    feat: добавить новый функционал серверной части

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.post(
       password: Joi.string().required(),
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
-      avatar: Joi.string().domain(),
+      avatar: Joi.string().domain({allowFullyQualified: true}),
     }),
   }),
   createUser,
