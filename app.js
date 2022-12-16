b939c47aa867a84ed656d4a2ed869ca7090a6a67
Anatolii Rotckin <rokkker@yandex.ru>
2022-12-16 15:48:45 +0200

    feat: добавить новый функционал серверной части

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.post(
   '/signin',
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string().required(),
+      email: Joi.string().email().required(),
       password: Joi.string().required(),
     }),
   }),
@@ -34,9 +34,7 @@ app.post(
       password: Joi.string().required(),
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
-      avatar: Joi.string().uri({
-        scheme: /^((http|https):\/\/)(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/gm
-      }),
+      avatar: Joi.string().uri(),
     }),
   }),
   createUser,
